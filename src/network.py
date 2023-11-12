817fe622164a9842f067e79b68a7b15c1411032d
Daniel Vakhrushev <45727078+danielpancake@users.noreply.github.com>
2023-11-12 23:18:22 +0300

    Added network

---
+++ src/network.py
@@ -0,0 +1,104 @@
+import numpy as np
+
+from tqdm import tqdm
+
+if __name__ == "__main__":
+    from core import LayerWithParams, LossFunction
+else:
+    from .core import LayerWithParams, LossFunction
+
+
+class Network:
+    def __init__(self, layers: list, loss):
+        self.layers = layers
+        self.layers_trainable = [
+            layer for layer in layers if isinstance(layer, LayerWithParams)
+        ]
+
+        self.loss = loss
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        for layer in self.layers:
+            x = layer.forward(x)
+        return x
+
+    def backward(self, grad: np.ndarray) -> np.ndarray:
+        for layer in reversed(self.layers):
+            grad = layer.backward(grad)
+        return grad
+
+    def update(self, lr: float):
+        for layer in self.layers_trainable:
+            layer.update(lr)
+
+    @staticmethod
+    def split_train_val(x: np.ndarray, y: np.ndarray, train_val_split: float) -> tuple:
+        if train_val_split <= 0 or train_val_split >= 1 or np.isnan(train_val_split):
+            raise ValueError("train_val_split must be between 0 and 1")
+
+        train_val_split_mask = np.random.rand(x.shape[0]) < train_val_split
+
+        x_train, y_train = x[train_val_split_mask], y[train_val_split_mask]
+        x_val, y_val = x[~train_val_split_mask], y[~train_val_split_mask]
+
+        return x_train, y_train, x_val, y_val
+
+    def train_step(self, x: np.ndarray, y: np.ndarray, lr: float) -> float:
+        out = self.forward(x)
+
+        loss = self.loss(out, y)
+        grad = self.loss.backward()
+
+        self.backward(grad)
+        self.update(lr)
+
+        return loss
+
+    def train(
+        self,
+        x: np.ndarray,
+        y: np.ndarray,
+        lr: float,
+        epochs: int,
+        train_val_split: float = 0.8,
+        shuffle: bool = False,
+    ):
+        # Shuffle dataset
+        if shuffle:
+            shuffle_mask = np.random.permutation(len(x))
+            x = x[shuffle_mask]
+            y = y[shuffle_mask]
+
+        # Split into training and validation set
+        x_train, y_train, x_val, y_val = self.split_train_val(x, y, train_val_split)
+        x_train_size, x_val_size = x_train.shape[0], x_val.shape[0]
+
+        for epoch in range(epochs):
+            print(f"Epoch {epoch + 1}/{epochs}")
+
+            train_losses = []
+            e_tqdm = tqdm(range(x_train_size))
+
+            for i in e_tqdm:
+                train_loss = self.train_step(x_train[i], y_train[i], lr)
+                train_losses.append(train_loss)
+
+                if i % 1000 == 0:
+                    e_tqdm.set_postfix({"loss": np.mean(train_losses)})
+
+            # Validation
+            if x_val_size > 0:
+                print("Evaluating...", end=" ")
+                acc = self.evaluate(x_val, y_val)
+                print(f"Validation accuracy: {acc * 100:.2f}%")
+
+            e_tqdm.close()
+
+    def evaluate(self, x: np.ndarray, y: np.ndarray) -> float:
+        correct = 0
+
+        for i in range(len(x)):
+            out = self.forward(x[i])
+            correct += np.argmax(out) == np.argmax(y[i])
+
+        return correct / len(x)
